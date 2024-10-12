07db3e323d2592b695ad968ff575ca2566eefc40
Manuel Chinchi <manuel.chinchi93@gmail.com>
2024-10-12 11:58:59 -0300

    add application template 'maui'

---
+++ crud-maui-android/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace crud_maui_android
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ crud-maui-android/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace crud_maui_android
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
